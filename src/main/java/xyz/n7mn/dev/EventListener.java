6073a046fb935384147d3dca6d4931e9ce27e965
kanasaki15 <kassi_0123@kassi-hp.info>
2020-11-30 03:01:19 +0900

    Initial commit

---
+++ src/main/java/xyz/n7mn/dev/EventListener.java
@@ -0,0 +1,15 @@
+package xyz.n7mn.dev;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+class EventListener extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+
+
+    }
+
+}
---
+++ src/main/java/xyz/n7mn/dev/NanaminBot.java
@@ -0,0 +1,22 @@
+package xyz.n7mn.dev;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class NanaminBot {
+
+    public static void main(String[] args) {
+
+        try {
+            JDABuilder.createLight("NzgxMzIzMDg2NjI0NDU2NzM1.X7791A.swaLPRnfZzoL50ntQ7QmC-DqjxQ", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+                    .addEventListeners(new EventListener())
+                    .setActivity(Activity.watching("Discord"))
+                    .build();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
