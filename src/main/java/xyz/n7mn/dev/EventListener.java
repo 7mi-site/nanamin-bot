9b070960fb67fed7ba353e0592a4a1b53e2de1f0
kanasaki15 <kassi_0123@kassi-hp.info>
2020-11-30 03:45:21 +0900

    さいころ、投票ヘルプ追加

--- src/main/java/xyz/n7mn/dev/EventListener.java
+++ src/main/java/xyz/n7mn/dev/EventListener.java
@@ -1,15 +1,78 @@
 package xyz.n7mn.dev;
 
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.RestAction;
+
+import java.util.List;
+import java.util.Random;
 
 class EventListener extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
 
+        User author = event.getAuthor();
+        String text = event.getMessage().getContentRaw();
+
+        if (author.isBot()){
+            return;
+        }
 
 
+        if (text.toLowerCase().startsWith("n.")){
+            System.out.println("---- Debug ----\n" + author.getAsTag() + "\n" + text + "\n----- Debug -----");
+        }
+
+
+        if (text.toLowerCase().equals("n.dice")){
+
+            int i = new Random().nextInt(5) + 1;
+            event.getMessage().getTextChannel().sendMessage("さいころの結果は" + i + "です。").queue();
+
+        }
+
+        if (text.toLowerCase().equals("n.vote")){
+
+            TextChannel textChannel = event.getTextChannel();
+            List<Member> members = textChannel.getMembers();
+            for (Member member : members){
+
+                if (member.getUser().getAsTag().equals("n7mn-VoteBot#6771")){
+                    return;
+                }
+
+            }
+
+            Message message = event.getMessage();
+            message.delete().queue();
+
+            String msg = message.getAuthor().getAsTag()+"さんっ！\n" +
+                    "n.voteのヘルプですっ！\n" +
+                    "コマンドの書き方は\n" +
+                    "`n.vote <タイトル> <選択肢1> <選択肢2> <選択肢3> <...> <選択肢20>` または\n" +
+                    "`n.vote\n<タイトル>\n<選択肢1>\n<選択肢2>\n<選択肢3>\n<...>\n<選択肢20>`\nですっ！\n" +
+                    "タイトルが必要じゃない場合はn.voteNtとつけて同じようにしてくださいっ！！";
+
+            event.getMessage().getTextChannel().sendMessage(msg).queue();
+
+
+        }
+
+        if (text.toLowerCase().startsWith("n.vote")){
+
+            TextChannel textChannel = event.getTextChannel();
+            List<Member> members = textChannel.getMembers();
+            for (Member member : members){
+
+                if (member.getUser().getAsTag().equals("n7mn-VoteBot#6771")){
+                    return;
+                }
+
+            }
+
+        }
     }
 
 }
