fd288bf4a5bc85a05a8021b095390c18b77ee767
kanasaki15 <kassi_0123@kassi-hp.info>
2020-12-16 23:02:42 +0900

    サボってたヘルプ更新

--- src/main/java/xyz/n7mn/dev/ChatMessage.java
+++ src/main/java/xyz/n7mn/dev/ChatMessage.java
@@ -177,19 +177,36 @@ public class ChatMessage {
                 "**※このメッセージは送信専用です。返信しても何もできませんのでご注意ください。**\n" +
                 "**※バグの報告、要望などは https://discord.gg/QP2hRSQaVV までお願いします。**\n" +
                 "`n.vote`、`n.voteNt` -- アンケートを表示する(詳細はn.voteと打って詳細ヘルプを出してください)\n" +
+                "`n.voteStop <メッセージリンクのURL>` -- アンケートを終了する" +
                 "`n.ping` -- 応答を返す\n"+
                 "`n.nullpo` または `n.ぬるぽ` -- ガッ\n"+
                 "`n.dice` -- さいころを振る\n"+
                 "`n.random <文字列1> <文字列2> <...> <文字列n>` -- 指定された文字列の中から一つを表示する\n" +
                 "`n.play <URL>` -- 音楽を再生する(停止する場合は`n.stop`)\n" +
-                "`n.check` -- 動作確認\n\n" +
-                "--- 地震情報機能について ---\n" +
-                "地震情報機能は「nanami_setting」という名前でチャンネルを作成し\n" +
-                "「jisin: <情報を流したいテキストチャンネルのID>」とメッセージを送ってください。\n\n" +
-                "----- ななみちゃんbot ヘルプ End ----";
+                "`n.fire` -- :fire:\n" +
+                "`n.burst` -- どっかーん\n" +
+                "`n.check` -- 動作確認\n";
+        StringBuffer sb = new StringBuffer(helpText);
+
+        if (!message.getGuild().getId().equals("517669763556704258")){
+            sb.append("--- 地震情報機能について ---\n"+
+                    "地震情報機能は「nanami_setting」という名前でチャンネルを作成し\n" +
+                    "「jisin: <情報を流したいテキストチャンネルのID>」とメッセージを送ってください。\n" +
+                    "----- ななみちゃんbot ヘルプ End ----");
+        } else {
+            sb.append("---- ゆるり鯖Discord限定機能 ----\n" +
+                    "`n.gold` -- こうたちゃんの金装備は神！\n" +
+                    "`n.kouta` -- 金装備のこうたさん\n" +
+                    "`n.れにょこ` or `n.renyoko` or `n.ﾚﾆｮｺ` or `n.レニョコ` -- 虫特攻はいい！\n" +
+                    "`n.sand` -- Crousandさん\n" +
+                    "`n.sc` -- 架空のスパチャを送る\n" +
+                    "`n.pan` -- パンマスター" +
+                    "`n.hentai` -- へんたいっ！\n\n" +
+                    "----- ななみちゃんbot ヘルプ End ----");
+        }
 
         PrivateChannel sendUserDM = author.openPrivateChannel().complete();
-        sendUserDM.sendMessage(helpText).queue();
+        sendUserDM.sendMessage(sb.toString()).queue();
 
         message.reply("ななみちゃんbotのヘルプをDMにお送りいたしましたっ！").queue();
 
