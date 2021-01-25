af9cdc20f30b4e811677c280dbf2d68817a94845
kanasaki15 <kassi_0123@kassi-hp.info>
2021-01-25 14:03:50 +0900

    人狼ゲームつくりかけ(役職クラスの原型のみ完成)

---
+++ src/main/java/xyz/n7mn/dev/Command/game/jinro/JinroGame.java
@@ -0,0 +1,4 @@
+package xyz.n7mn.dev.Command.game.jinro;
+
+public class JinroGame {
+}
---
+++ src/main/java/xyz/n7mn/dev/Command/game/jinro/PlayerClass.java
@@ -0,0 +1,9 @@
+package xyz.n7mn.dev.Command.game.jinro;
+
+public interface PlayerClass {
+
+    String getClassName();
+    void run();
+    boolean isWerewolf();
+
+}
