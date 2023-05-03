e4726b142832cf085f2ffc3857b036cdb6c7df89
tina-chan <kassi_0123@kassi-hp.info>
2023-05-03 13:33:35 +0900

    ヘルプも音楽や投票と同じようにする

--- src/main/java/xyz/n7mn/dev/EventListener.java
+++ src/main/java/xyz/n7mn/dev/EventListener.java
@@ -266,8 +266,6 @@ public class EventListener extends ListenerAdapter {
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event){
-
-        MessageChannelUnion channel = event.getChannel();
         Member member = event.getMember();
         JDA jda = event.getJDA();
 
@@ -323,39 +321,7 @@ public class EventListener extends ListenerAdapter {
         }
 
         if (event.getFullCommandName().equals("help")){
-
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.setColor(Color.PINK);
-            builder.setTitle("ななみちゃんbot ヘルプ");
-            builder.setDescription("" +
-                    "質問や要望、バグ報告は https://discord.gg/FnjCMzP7d4 までお願いします。\n" +
-                    (ver.get().matches(".*dev.*") ? "**このBotはベータ版です！予告なしで停止する場合があります！！*" : "")
-            );
-
-            builder.addField("/help", "いまあなたが見ているやつですっ！！", false);
-            // builder.addField("/vote", "投票機能\n\n例\n/vote 朝ごはん食べる？ 2024-12-01 00:00:00 食べる 食べない なにそれ？ ご飯を食べられない人だっているんですよ！！！ それってあなたの感想ですよね？", false);
-            // builder.addField("/game", "ミニゲーム機能\n\n実装し直し中です。しばらく待っててね！", false);
-            builder.addField("/music", "音楽再生機能", false);
-            builder.addField("/nanami-version", "バージョン情報を出す。\nバグ報告に役にたつのでそのときはお願いしますっ", false);
-            //builder.addField("/nanami-setting", "ななみちゃんbotの設定ができるコマンドです。 \n昔のnanami_settingチャンネルの代わりですっ", false);
-
-            OptionMapping option = event.getOption("送信方式");
-
-            if (option != null && option.getAsString().equals("d")){
-                jda.getUserById(member.getId()).openPrivateChannel().queue((s->{
-                    s.sendMessageEmbeds(builder.build()).queue();
-                    event.reply("ヘルプをDMに送信しましたっ！").setEphemeral(true).queue();
-                }));
-
-                return;
-            }
-
-            if (option != null && option.getAsString().equals("a")){
-                event.replyEmbeds(builder.build()).setEphemeral(false).queue();
-                return;
-            }
-
-            event.replyEmbeds(builder.build()).setEphemeral(true).queue();
+            new Help(jda, event).run();
         }
 
         if (event.getFullCommandName().equals("music")){
