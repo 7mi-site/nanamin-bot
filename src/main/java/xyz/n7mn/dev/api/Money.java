84247e76b3d986c9a605e9986786b4dbac5ac360
tina-chan <kassi_0123@kassi-hp.info>
2023-05-11 00:30:12 +0900

    おかねシステム

--- src/main/java/xyz/n7mn/dev/api/Money.java
+++ src/main/java/xyz/n7mn/dev/api/Money.java
@@ -1,4 +1,123 @@
 package xyz.n7mn.dev.api;
 
+import com.amihaiemil.eoyaml.Yaml;
+import com.amihaiemil.eoyaml.YamlMapping;
+import com.amihaiemil.eoyaml.YamlMappingBuilder;
+import org.jetbrains.annotations.Nullable;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Protocol;
+import xyz.n7mn.dev.money.MoneyData;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Money {
+
+    private static final File config = new File("./config-redis.yml");
+    private static long money = 100L;
+
+    @Nullable
+    public static Long get(String memberId){
+        YamlMapping ConfigYml = null;
+        try {
+            if (!config.exists()){
+                config.createNewFile();
+
+                YamlMappingBuilder builder = Yaml.createYamlMappingBuilder();
+                ConfigYml = builder.add(
+                        "RedisServer", "127.0.0.1"
+                ).add(
+                        "RedisPort", String.valueOf(Protocol.DEFAULT_PORT)
+                ).add(
+                        "RedisPass", ""
+                ).build();
+
+                try {
+                    PrintWriter writer = new PrintWriter(config);
+                    writer.print(ConfigYml.toString());
+                    writer.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                ConfigYml = Yaml.createYamlInput(config).readYamlMapping();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.gc();
+            return null;
+        }
+
+        JedisPool pool = new JedisPool(ConfigYml.string("RedisServer"), ConfigYml.integer("RedisPort"));
+        Jedis jedis = pool.getResource();
+        jedis.auth(ConfigYml.string("RedisPass"));
+
+        try {
+            money = Long.parseLong(jedis.get("nanamibot:money:" + memberId));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        jedis.close();
+        pool.close();
+
+        System.gc();
+
+        return money;
+    }
+
+    public static void set(String memberId, Long money){
+        System.gc();
+
+        YamlMapping ConfigYml = null;
+        try {
+            if (!config.exists()){
+                config.createNewFile();
+
+                YamlMappingBuilder builder = Yaml.createYamlMappingBuilder();
+                ConfigYml = builder.add(
+                        "RedisServer", "127.0.0.1"
+                ).add(
+                        "RedisPort", String.valueOf(Protocol.DEFAULT_PORT)
+                ).add(
+                        "RedisPass", ""
+                ).build();
+
+                try {
+                    PrintWriter writer = new PrintWriter(config);
+                    writer.print(ConfigYml.toString());
+                    writer.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                ConfigYml = Yaml.createYamlInput(config).readYamlMapping();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.gc();
+            return;
+        }
+
+        JedisPool pool = new JedisPool(ConfigYml.string("RedisServer"), ConfigYml.integer("RedisPort"));
+        Jedis jedis = pool.getResource();
+        jedis.auth(ConfigYml.string("RedisPass"));
+
+        if (money != null){
+            jedis.set("nanamibot:money:" + memberId, money.toString());
+        } else {
+            jedis.set("nanamibot:money:" + memberId, "100");
+        }
+
+        jedis.close();
+        pool.close();
+
+        System.gc();
+    }
+
 }
